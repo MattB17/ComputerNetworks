f6469aa581eed303debac0524901c0f73da5431c
MattB17 <matt.buckley1755@gmail.com>
2020-08-13 21:27:20 -0400

    Implementing the RTT calculation for the client

--- Networking/UDPPinger/udp_client.py
+++ Networking/UDPPinger/udp_client.py
@@ -4,14 +4,21 @@ from datetime import datetime
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.settimeout(1)
 
-server_name = ''
+server_name = '127.0.0.1'
 server_port = 12000
 
 for i in range(10):
     message = "Ping {0} {1}".format(i+1, datetime.now())
-    client_socket.sendto(message, (server_name, server_port))
+    client_socket.sendto(message.encode(), (server_name, server_port))
     try:
         modified_message, server_address = client_socket.recvfrom(1024)
+        modified_message = modified_message.decode()
         print(modified_message)
+        sent_time_str = " ".join(modified_message.split()[2:])
+        sent_time = datetime.strptime(sent_time_str, "%Y-%m-%d %H:%M:%S.%f")
+        rtt = int(str(datetime.now() - sent_time).split(".")[1])
+        print("RTT of {} ms".format(rtt))
     except socket.timeout:
         print("Request timed out")
+    if i < 9:
+        print("------------------")
--- Networking/UDPPinger/udp_server.py
+++ Networking/UDPPinger/udp_server.py
@@ -2,11 +2,14 @@ import random
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('', 12000))
+server_socket.bind(('127.0.0.1', 12000))
 
 while True:
     rand = random.randint(0, 10)
     message, address = server_socket.recvfrom(1024)
-    message = message.upper()
+    message = message.decode().upper()
+    message_lst = message.split()
+    message_lst[0] = "PONG"
+    message = " ".join(message_lst)
     if rand >= 4:
-        server_socket.sendto(message, address)
+        server_socket.sendto(message.encode(), address)
