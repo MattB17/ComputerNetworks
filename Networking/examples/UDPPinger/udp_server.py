f5df3733c7fff3ab52d87193633d91cdb032ef52
MattB17 <matt.buckley1755@gmail.com>
2020-08-20 19:03:46 -0400

    Updating udp_server.py example to use UDPServer class

--- Networking/examples/UDPPinger/udp_server.py
+++ Networking/examples/UDPPinger/udp_server.py
@@ -1,15 +1,5 @@
-import random
-import socket
+from Networking.UDPPinger import UDPServer
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(('127.0.0.1', 12000))
-
-while True:
-    rand = random.randint(0, 10)
-    message, address = server_socket.recvfrom(1024)
-    message = message.decode().upper()
-    message_lst = message.split()
-    message_lst[0] = "PONG"
-    message = " ".join(message_lst)
-    if rand >= 4:
-        server_socket.sendto(message.encode(), address)
+if __name__ == "__main__":
+    server = UDPServer('', 12000, 0.7)
+    server.run()
